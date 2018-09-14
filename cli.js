355b9d734e2299bed74f732a1429124540e5c457
chinanf-boy <865501259@qq.com>
2018-09-14 17:51:47 +0800

    Up two-log-min 1.0.6

--- cli.js
+++ cli.js
@@ -4,16 +4,10 @@
 const meow = require('meow');
 const twoLog = require('two-log-min');
 const tc = require('turbocolor');
-const c = tc.cyan
-const m = tc.magenta
-const r = tc.red
-let userUse = api => {
-	let createDebug = api.log;
-	createDebug.formatters.h = (v) => {
-	return v.toString('hex')
-	}
-};
-const toS = (s) => JSON.stringify(s,null,0)
+const c = tc.cyan;
+const m = tc.magenta;
+const r = tc.red;
+const toS = s => JSON.stringify(s, null, 0);
 
 const cli = meow(`
 	Usage
@@ -29,29 +23,37 @@ const cli = meow(`
 	  winston show
 `);
 
-let D = cli.flags['D'] || false
-let l = twoLog(D,userUse);
+let D = cli.flags['D'] || false;
+let userUse = api => {
+	let createDebug = api.log;
+	createDebug.formatters.h = v => {
+		return v.toString('hex');
+	};
+};
+let l = twoLog(D, userUse);
 let t = 6000;
 
 let useWhat = !D ? 'ora' : 'log';
 
 const o1 = {
 	ora: 'red',
-	log: 'cli'
-}
+	log: 'cli',
+};
 
-const backLog = l.start(`1 debug:${D} , then use ${c(useWhat)} ${m(toS(o1))}`, o1);
+const backLog = l.start(
+	`1 debug:${D} , then use ${c(useWhat)} ${m(toS(o1))}`,
+	o1
+);
+const backLog2 = l.start(`2 debug`);
 
 setTimeout(() => {
-	backLog(`use backLog ${c('withou log namespace')}`);
+	backLog2(`use backLog ${c('withou log namespace')}`, { only: 'log' });
 }, t - 5000);
-
-const backLog2 = l.start(`2 debug`);
 
 const o2 = {
 	ora: 'green',
-	log: 'cli'
-}
+	log: 'cli',
+};
 
 setTimeout(() => {
 	l.text(`ora:green, log:cli %h`, new Buffer('hello world'), o2);
@@ -59,11 +61,11 @@ setTimeout(() => {
 	l.one('one time ora');
 }, t - 3000);
 
-const o3 =  {
+const o3 = {
 	ora: 'green',
 	log: 'cli',
 	only: 'log',
-}
+};
 
 setTimeout(() => {
 	l.text(`ora:green, only show log`, o3);
@@ -71,15 +73,14 @@ setTimeout(() => {
 
 const o4 = {
 	ora: 'fail',
-	log: 'cli'
-}
+	log: 'cli',
+};
 
 setTimeout(() => {
 	l.stop(`i fail if ora `, o4);
 }, t);
 
-
-process.on('uncaughtException', function(err){
+process.on('uncaughtException', function(err) {
 	console.error('\n%s', r(err.stack));
 	process.exitCode = 1;
 });
